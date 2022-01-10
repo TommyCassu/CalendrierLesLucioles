879da7218935b9edb93656b68b307695e6a709a2
mathbroche <mathis.broche.sio2020@gmail.com>
2022-01-10 16:20:47 +0100

    Redirect

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/main', name: 'main')]
-    public function index(): Response
+    public function index(UserRepository $user): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
