34175c36af685be0e63811b37526a5dc65a83471
mathbroche <mathis.broche.sio2020@gmail.com>
2022-01-12 14:13:28 +0100

    [WIP] EasyAdmin Re-skin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,15 +3,39 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
 {
+    
     public static function getEntityFqcn(): string
     {
         return User::class;
     }
 
+
+    public function configureActions(Actions $actions):Actions
+    {
+        //Création d'une nouvelle action
+        $redirect = Action::new('redirect','Editer')
+            //indique quelle méthode suivre 
+            ->linkToCrudAction('redirection')
+            ;
+        return $actions
+                //ajoute notre nouvelle action sur la page index (page principale)
+                ->add(Crud::PAGE_INDEX,$redirect)
+                ->remove(Crud::PAGE_INDEX,Action::EDIT)
+            // ->add(Crud::PAGE_INDEX, Action::EDIT)
+            ;
+    }
+    public function redirection(){
+        //redirige vers le main
+        return $this->redirectToRoute('app_login');
+    }
     /*
     public function configureFields(string $pageName): iterable
     {
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,4 +15,61 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
+
+    #[Route('/edit', name: 'edit' )]
+    public function edit(Request $request, Producteur $producteur, UserPasswordEncoderInterface $userPass): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        //Edition Nom
+        if ($request->request->get('nom') != null) {
+            $producteur->setNom($request->request->get('nom'));
+        }
+
+        //Edition Prenom
+        if ($request->request->get('username') != null) {
+            $producteur->setPrenom($request->request->get('username'));
+        }
+
+        //Edition Telephone
+        if ($request->request->get('tel') != null) {
+            $producteur->setTel($request->request->get('tel'));
+        }
+
+        //Edition Email
+        if ($request->request->get('mail') != null) {
+            $producteur->setMail($request->request->get('mail'));
+        }
+
+        //Verification mot de passe
+        if ($request->request->get('passAnc') != null) {
+            $oldPassword = $request->request->get('passAnc');
+            $newPass = $request->request->get('pass');
+            $newPassConf = $request->request->get('passConf');
+            if ($userPass->isPasswordValid($producteur,$oldPassword)){
+                if ($newPass != null){
+                    if ($newPass == $newPassConf){
+                        if ($newPass != $oldPassword ){
+                            $passHash = ($userPass->encodePassword($producteur, $newPass)); 
+                            $producteur->setMdp($passHash);
+                        }else{
+                            $this->addFlash(
+                                'alert',
+                                'Votre nouveau mot de passe ne peut pas être identique a votre mot de passe actuel'
+                            );
+                        }
+                    }else{
+                        $this->addFlash(
+                            'alert',
+                            'Veuillez entrez deux mot de passe identique'
+                        );
+                    }
+                }else{
+                    $this->addFlash(
+                        'alert',
+                        'Veuillez rentrer le nouveau mot de passe'
+                    );
+                }
+            }
+        }
 }
