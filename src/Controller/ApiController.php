3b577b4a834907ab301226c47a1418f840b81e7e
TommyCassu <tommy.cassu.sio2020@gmail.com>
2022-01-12 13:03:59 +0100

    Added event list on the left side of the calendar

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -19,9 +19,57 @@ class ApiController extends AbstractController
             'controller_name' => 'ApiController',
         ]);
     }
+    
+    #[Route('/api/create', name: 'api_event_create', methods: ['POST'])]
+    public function createEvent(?Calendar $calendar, Request $request, EntityManagerInterface $em)
+    {
+        // on récupère les données
+        $donnees = json_decode($request->getContent(), true, 512, 0);
+
+        if (
+            1 + 1 == 2
+        ) {
+            //les données sont complètes
+            //On initialise un code
+            $code = 200;
+
+            //On vérifie si l'id existe
+            if (!$calendar) {
+                // On instancie un rendez vous
+                $calendar = new Calendar;
+
+                //On change le code
+                $code = 201;
+            }
+
+            //On hydrate l'objet avec les données
+            $dateStart = new \DateTime($donnees["start"]);
+
+            $calendar->setTitle($donnees["title"]);
+            $calendar->setDescription($donnees["description"]);
+            $calendar->setStart($dateStart);
+            $calendar->setAllDay($donnees["allDay"]);
+            $calendar->setBackgroundColor($donnees["backgroundColor"]);
+            $calendar->setBorderColor($donnees["borderColor"]);
+            $calendar->setTextColor($donnees["textColor"]);
+
+            $em->persist($calendar);
+            $em->flush();
+
+            // On retourne le code
+            return new Response('Ok', $code);
+        } else {
+            //les données sont incomplètes
+            return new Response('Données incomplètes', 404);
+        }
+
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
 
     #[Route('/api/{id}/edit', name: 'api_event_edit', methods: ['GET', 'POST'])]
-    public function majEvent(?Calendar $calendar, Request $request, EntityManagerInterface $em,User $user)
+    public function majEvent(?Calendar $calendar, Request $request, EntityManagerInterface $em)
     {
         // on récupère les données
         $donnees = json_decode($request->getContent(), true, 512, 0);
