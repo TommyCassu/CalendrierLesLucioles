4dbecd8598a85f94682413b3d9b05a8ee987e9da
mathbroche <mathis.broche.sio2020@gmail.com>
2022-01-25 09:49:17 +0100

    Reminder Mailing System

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class MailController extends AbstractController
@@ -15,30 +16,46 @@ class MailController extends AbstractController
     #[Route('/mail', name: 'mail')]
     public function index(): Response
     {
+        
+        
         return $this->render('mail/index.html.twig', [
             'controller_name' => 'MailController',
         ]);
     }
 
-    #[Route('/sendLogins', name: 'sendLogins' )]
-    public function sendLogins(MailerInterface $mailer,UserRepository $user): Response
+    #[Route('/sendRappel', name: 'sendRappel' )]
+    public function sendRappel(MailerInterface $mailer,UserRepository $user): Response
     {
+
         $users =$user->findAll();
-        
         foreach ($users as $user){
-        $email = (new Email())
+            $tableaugarde=[];
+            foreach($user->getCalendars() as $calendar){
+                if($calendar->getStart()->format("Y-m-d H:i:s") > date("Y-m-d H:i:s")){
+                    $date = $calendar->getStart();
+                    $tableaugarde[] = [
+                        'date' => $date,
+                    ];
+                }
+            }
+                
+            $email = (new TemplatedEmail())
             ->from('mathbroche@gmail.com')
             ->to($user->getEmail())
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Voici vos identifiants !')
-            ->html('<p class="text-2xl">Votre nom est '.$user->getEmail().'</p>
-            <br>'.'Votre mot de passe est '.$user->getPassword().''
-            );
+            ->subject('Voici le rappel de vos gardes !')
+            // path of the Twig template to render
+            ->htmlTemplate('mail/rappel.html.twig')
+            // pass variables (name => value) to the template
+            ->context([
+                'tableaugarde' => $tableaugarde,
+                'username' => $user->getUsername(),
+            ])
+        ;
 
-        $mailer->send($email);
+            
+            $mailer->send($email);
+                
+            
         }
         return $this->redirectToRoute("main");
         // ...
